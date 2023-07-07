836baaec65db996fa079eec93517cde782066398
damir <dimayu82@gmail.com>
2023-07-07 14:16:19 +0300

    Add vite-plugin-webp, stylelint

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import { resolve } from 'path';
 import handlebars from "vite-plugin-handlebars";
 import babel from 'rollup-plugin-babel';
 import eslint from 'vite-plugin-eslint';
+import stylelint from 'vite-plugin-stylelint';
 import autoprefixer from 'autoprefixer';
+import webp from 'vite-plugin-webp';
 
 export default defineConfig({
   base: './',
@@ -21,6 +23,13 @@ export default defineConfig({
       partialDirectory: resolve(__dirname, "src/components"),
     }),
     eslint(),
+    stylelint({
+      fix: true,
+    }),
+    webp({
+      onlyWebp: resolve(__dirname, 'src/img'),
+      imageType: ['.png', '.jpg']
+    }),
   ],
   css: {
     postcss: {
